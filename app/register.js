7910ba9f8fe3f4c2ed3026f6a87644e118f4b3c0
azkafargha <azkafarghaa@gmail.com>
2024-12-16 10:41:27 +0700

    added TnC Modal

--- app/register.js
+++ app/register.js
@@ -1,54 +1,193 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import {
+  Alert,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import Button from "../components/Button";
-import Input from "../components/Input";
-import { Link } from "expo-router";
+import Checkbox from "expo-checkbox";
+import { useState } from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+// import Input from "../components/Input";
+// import { Link } from "expo-router";
 
 export default function App() {
+  const [isChecked, setChecked] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Image source={require("../assets/logo.png")} style={styles.logo} />
+        <View style={styles.container}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <ScrollView style={{ marginBottom: 10 }}>
+                  <View>
+                    <Text style={styles.title}>Terms and Conditions</Text>
+                  </View>
+                  <Text style={styles.modalText}>
+                    General Site Usage Last Revised: December 16, 2013 Welcome
+                    to www.lorem-ipsum.info. This site is provided as a service
+                    to our visitors and may be used for informational purposes
+                    only. Because the Terms and Conditions contain legal
+                    obligations, please read them carefully. 1. YOUR AGREEMENT
+                    By using this Site, you agree to be bound by, and to comply
+                    with, these Terms and Conditions. If you do not agree to
+                    these Terms and Conditions, please do not use this site.
+                    PLEASE NOTE: We reserve the right, at our sole discretion,
+                    to change, modify or otherwise alter these Terms and
+                    Conditions at any time. Unless otherwise indicated,
+                    amendments will become effective immediately. Please review
+                    these Terms and Conditions periodically. Your continued use
+                    of the Site following the posting of changes and/or
+                    modifications will constitute your acceptance of the revised
+                    Terms and Conditions and the reasonableness of these
+                    standards for notice of changes. For your information, this
+                    page was last updated as of the date at the top of these
+                    terms and conditions. 2. PRIVACY Please review our Privacy
+                    Policy, which also governs your visit to this Site, to
+                    understand our practices. 3. LINKED SITES This Site may
+                    contain links to other independent third-party Web sites
+                    ("Linked Sites”). These Linked Sites are provided solely as
+                    a convenience to our visitors. Such Linked Sites are not
+                    under our control, and we are not responsible for and does
+                    not endorse the content of such Linked Sites, including any
+                    information or materials contained on such Linked Sites. You
+                    will need to make your own independent judgment regarding
+                    your interaction with these Linked Sites.
+                  </Text>
+                </ScrollView>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                    setChecked(true);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Setuju boy</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nama Lengkap"
-        placeholderTextColor="#aaa"
-        
-      />
+          <Image source={require("../assets/logo.png")} style={styles.logo} />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        placeholderTextColor="#aaa"
-        keyboardType="email-address"
-        
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        placeholderTextColor="#aaa"
-        secureTextEntry={true}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Avatar URL"
-        placeholderTextColor="#aaa"
-        secureTextEntry={true}
-      />
+          <TextInput
+            style={styles.input}
+            placeholder="Nama Lengkap"
+            placeholderTextColor="#aaa"
+          />
 
-      
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="#aaa"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="#aaa"
+            secureTextEntry={true}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Avatar URL"
+            placeholderTextColor="#aaa"
+            secureTextEntry={true}
+          />
+          <View style={styles.checkbox}>
+            <Checkbox
+              value={isChecked}
+              onValueChange={setChecked}
+              color={isChecked ? "#228cb2" : undefined}
+            />
 
-      <Button text="Login" />
-      
-      
+            <Pressable onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.text}>
+                I have read{" "}
+                <Text style={styles.linktext}>terms and conditions</Text>
+              </Text>
+            </Pressable>
+          </View>
 
-      <StatusBar style="auto" />
-    </View>
+          <Button text="Login" />
+
+          <StatusBar style="auto" />
+        </View>
   );
 }
 
 const styles = StyleSheet.create({
+  linktext: {
+    color: "#1ebf5e",
+    fontWeight: "700",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  modalView: {
+    margin: 29,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#0a7465",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 100,
+    textAlign: "justify",
+  },
+  text: {
+    flexDirection: "row",
+    lineHeight: 24,
+    margin: 10,
+    alignItems: "justify",
+  },
+  checkbox: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginBottom: 10,
+  },
   container: {
+    
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
@@ -64,6 +203,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+    textAlign: "center",
   },
   input: {
     width: "100%",
@@ -72,7 +212,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 15,
     paddingHorizontal: 10,
-    marginBottom: 15,
+    marginBottom: 10,
     backgroundColor: "#f9f9f9",
     fontSize: 16,
   },
